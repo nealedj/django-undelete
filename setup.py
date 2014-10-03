af63407690faf4fa80ca657c014ef66b32dd1564
David Neale <davneale@google.com>
2014-10-03 14:13:31 +0100

    Bump version and only support >=Django 1.7

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name = "django-undelete",
-    version = "1.0.0",
+    version = "1.1.0",
     packages = find_packages(),
-    install_requires = ['django>=1.3'],
+    install_requires = ['django>=1.7'],
 )
 
